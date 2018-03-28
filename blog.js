d8a4a4735c35f7753d6d43018fa8499bbdaa18bf
YOUR-USERNAME <inisdad1@yahoo.com>
2018-03-27 23:37:38 -0600

    made button

--- blog.js
+++ blog.js
@@ -1,1 +1,13 @@
 //alert("up and running");
+let form = document.getElementById("blogForm");
+
+form.addEventListener("Submit", function(e)
+	{
+		e.preventDefault()
+		const inputValue = document.getElementById
+		("blogInput").value;
+		
+	} );
+
+let newElement = document.create("p");
+newElement.inner.HTML = input;
